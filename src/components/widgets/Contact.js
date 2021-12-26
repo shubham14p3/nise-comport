ee7f8e4e33071b3693cd4a4b0003b1dbbcabe058
Shubham Raj <shubham14p3@gmail.com>
2021-12-26 22:13:10 +0530

    added Chatbot properties

--- src/components/widgets/Contact.js
+++ src/components/widgets/Contact.js
@@ -4,9 +4,9 @@ import UrlIcon from "../../assets/icons/call.svg";
 
 const ContactLink = () => {
     return (
-        <a href="tel:1999" className="chatbotbox-tel-link">
+        <a href="tel:+919835552756" className="chatbotbox-tel-link">
             <img className="chatbotbox-url-icon" alt="CallIcon" src={UrlIcon} />
-            <h1 className="tel-header"> Call 1999 </h1>
+            <h1 className="tel-header"> Call 9835552756 </h1>
         </a>
     );
 };
